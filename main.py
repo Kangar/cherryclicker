357b985e135314f1792f8ce0162ee2309b3191d6
lim0 <lim0@mailinator.com>
2015-04-17 22:25:07 +0300

    added static files dir and inline server config

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import cherrypy
-	  
-class CherryClicker(object):
-    def index(self):
-        return "Welcome to Cherry Clicker!"
-    index.exposed = True
+import os
 
-cherrypy.quickstart(CherryClicker())
+STATIC_DIR = os.path.join(os.getcwd(), 'public')
+
+class CherryClicker(object):
+	@cherrypy.expose
+	def index(self):
+		cherrypy.response.headers['Content-Type']= 'text/html'
+		return '(index page goes here)<br />' + \
+		'<a href="hello.txt">link to static file</a>'
+
+cherrypy.server.socket_port = 8080
+cherrypy.server.socket_host = '0.0.0.0'
+
+conf = {
+	'/': {
+		'tools.staticdir.on' : True,
+		'tools.staticdir.dir' : STATIC_DIR,
+		'tools.staticdir.index' : 'index.html'}
+}
+
+cherrypy.quickstart(CherryClicker(), config = conf)
